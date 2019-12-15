e46871fabfb6fb410b1312b58a1d937969f3ed83
Trofimov Eugene <evgentrofi@gmail.com>
2019-12-15 22:27:04 +0800

    Add project files.

---
+++ ToDoListMobile/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using ToDoListMobile.ViewModels;
+using Xamarin.Forms;
+
+namespace ToDoListMobile.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public  ICommand ClickCommand { get; private set; }
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ ToDoListMobile/Views/NotePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListMobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoListMobile.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotePage : ContentPage
+	{
+		public NoteViewModel ViewModel { get; private set; }
+		public NotePage(NoteViewModel vm)
+		{
+			InitializeComponent();
+			ViewModel = vm;
+			PickerPriority.Items.Add("Высокий");
+			PickerPriority.Items.Add("Средний");
+			PickerPriority.Items.Add("Низкий");
+			PickerPriority.Items.Add("Нет приоритета");
+			this.BindingContext = ViewModel;
+		}
+
+		private void OnSelectedIndexChanged(object sender, EventArgs e)
+		{
+			ViewModel.PriorityText = PickerPriority.Items[PickerPriority.SelectedIndex];
+		}
+	}
+}
